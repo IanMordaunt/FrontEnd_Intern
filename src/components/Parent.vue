8dd0a3ed8ab31f9552f714b8927e82a34bf23f62
Ian <new@news-MacBook-Pro.local>
2022-07-12 14:33:23 -0600

    added JSON local data, created set and get calls to local Storage

--- src/components/Parent.vue
+++ src/components/Parent.vue
@@ -1,39 +1,55 @@
 <script>
+import json from "./jsonData.json";
+
 export default {
   data() {
     return {
       newUser: "",
       users: [],
+      myJson: json,
     };
   },
 
   methods: {
     // Add a User
     addUser() {
-      this.users = [...this.users, this.newUser]
-      console.log('new user', this.users)
+      this.users = [...this.users, this.newUser];
+      console.log("new user", this.users);
 
       localStorage.setItem("myContent", JSON.stringify({ users: this.users }));
+    },
+    // Add all JSON Data to Local Storage
+    addData() {
+      console.log("new data", this.myJson);
 
+      localStorage.setItem("myData", JSON.stringify({ myJson: this.myJson }));
     },
 
     // Get All Users
     getUsers() {
-    const myContent = localStorage.getItem("myContent");
-    const data = JSON.parse(myContent)
-    this.users = data.users
+      const myContent = localStorage.getItem("myContent");
+      const data = JSON.parse(myContent);
+      this.users = data.users;
     },
+
+    getData() {
+      const myData = localStorage.getItem("myData");
+      const data = JSON.parse(myData);
+      this.myJson = data.myJson;
+    }
   },
 
   mounted() {
-
-    this.getUsers()
+    // this.getUsers();
+    // this.getData();
 
     window.onstorage = () => {
       // When local storage changes, dump the list to
       // the console.
-      this.getUsers()
-      console.log(JSON.parse(window.localStorage.getItem('myContent')));
+      this.getUsers();
+      this.getData();
+      console.log(JSON.parse(window.localStorage.getItem("myContent")));
+      console.log(JSON.parse(window.localStorage.getItem("myData")));
     };
   },
 };
@@ -48,8 +64,15 @@ export default {
   />
 
   <button @click="addUser">Add User</button>
+  <button @click="addData">Add Data</button>
+  <div>
+    <ul>
+      <li v-for="(user, index) in users" :key="index">{{ user }}</li>
+    </ul>
+  </div>
 
-  <ul>
-    <li v-for="(user, index) in users" :key="index">{{ user }}</li>
-  </ul>
+  <div>
+    <div v-for="(data, index) in myJson" :key="index"><p>{{ data.name }}</p></div>
+
+  </div>
 </template>
