cbb0f809df54920d4de699343724b759e431ad83
Ian <new@news-MacBook-Pro.local>
2022-07-20 10:30:25 -0600

    added New Window button and openIt() function in Parent.vue methods

--- src/components/Parent.vue
+++ src/components/Parent.vue
@@ -1,136 +1,27 @@
 <script>
 import axios from "axios";
-// import indexedDB from "../utilities/indexedDB-helper";
-
-
-// Create IndexedDB - Set DB-Name and DB-Version
-const DB_NAME = 'stateManager'
-const DB_VERSION = 2
-const TABLE_NAME = 'selections'
-let DB
-
-
-const getDb = async () => new Promise((resolve, reject) => {
-  console.log('call get DB')
-  if (DB) {
-    console.log('exists')
-    resolve(DB)
-  }
-
-  const request = indexedDB.open(DB_NAME, DB_VERSION)
-  console.log("REQUEST", request)
-
-  request.onerror = (event) => {
-    console.log('DB ERROR', event)
-    reject(`Error ${event}`)
-  }
-
-  request.onsuccess = (event) => {
-    console.log('DB SUCCESS', event.target.result)
-    DB = event.target.result
-    resolve(DB)
-  }
-
-  request.onupgradeneeded = (event) => {
-    console.log('DB ON UPDATE NEEDED')
-    const db = (event.target).result
-    try {
-      db.deleteObjectStore(TABLE_NAME)
-    } catch {
-
-    }
-    db.createObjectStore(TABLE_NAME, { keyPath: 'selectionId' })
-  }
-});
-
-const saveSelections = async (newSelections) => {
-const db = await getDb()
-  // eslint-disable-next-line no-async-promise-executor
-  return new Promise(async (resolve, reject) => {
-    try {
- 
-     const note = {
-        selectionId: 4,
-        selectionList: JSON.parse(JSON.stringify(newSelections))
-      } 
-
-      const trans = db.transaction(TABLE_NAME, 'readwrite')
-      const store = trans.objectStore(TABLE_NAME)
-
-      /* Remove observable data added by VUE
-    https://github.com/vuejs/Discussion/issues/292 */
-      // const cleanedUpData = JSON.parse(JSON.stringify(newSelections))
-
-    
-      console.log('ADD TO INDEXEDDB', note)
-      const result = await store.put(note)
-
-
-      // close database when transaction is complete
-      trans.oncomplete = () => {
-        db.close()
-      }
-
-      resolve(result)
-    } catch (error) {
-      // eslint-disable-next-line no-console
-      console.error(`There was an error adding to drafts, ${error}`)
-      reject(error)
-    }
-  })
-};
-
-const getSelections = async (selectionId) => {
-  const db = await getDb()
-  let selections = {} 
-
-  return new Promise((resolve, reject) => {
-    try {
-      const trans = db.transaction(TABLE_NAME, 'readonly')
-      const store = trans.objectStore(TABLE_NAME)
-      const request = store.get(9)
-
-
-      // assign results to selections when request has been successful
-      request.onsuccess = (event) => {
-        selections = event.target.result ?? selections
-        resolve(selections)
-      }
-
-      // close db when request is complete
-      request.oncomplete = () => {
-        db.close()
-      }
-    } catch (error) {
-      // eslint-disable-next-line no-console
-      console.error(`There was an error accessing drafts, ${error}`)
-      reject(error)
-    }
-  })
-};
-
-
+import { saveSelections, getSelections } from "../utilities/indexedDB-helper";
 
 export default {
   data() {
     return {
-
       myJson: {},
       selection: [],
       version: 0,
-      changedLocation: 'nowhere',
-      
+      changedLocation: "nowhere",
     };
   },
 
   methods: {
+    openIt() {
+      const url = "new_url";
+      window.open(url);
+    },
 
     updateVersion() {
       this.version += 1;
-      this.changedLocation = "here"
+      this.changedLocation = "here";
       console.log("latest version:", this.version);
-
-      console.log("new data", this.myJson);
 
       localStorage.setItem(
         "version",
@@ -141,103 +32,101 @@ export default {
     getVersion() {
       const unparsedJson = localStorage.getItem("version");
       const data = JSON.parse(unparsedJson);
-      console.log('get version', data)
+      console.log("get version", data);
 
-      return data?.version
+      return data?.version;
     },
 
-    async fetchData(){
-      try{
-        const res = await axios.get('http://localhost:5555/data')
-        console.log('response', res.data)
-        this.myJson = res.data
+    async fetchData() {
+      try {
+        const res = await axios.get("http://localhost:5555/data");
+        console.log("response", res.data);
+        this.myJson = res.data;
       } catch (e) {
-        console.log('error', e)
+        console.log("error", e);
       }
     },
 
-    createSelectionObject(){
+    createSelectionObject() {
       this.selection = this.myJson.map((item) => {
-        console.log({id: item.id})
-        return {id: item.id, selected: false}
-        
-      })
+        console.log({ id: item.id });
+        return { id: item.id, selected: false };
+      });
     },
 
-    toggleItemSelection(id){
-      const foundItem = this.selection.find(item => item.id === id)
-      foundItem.selected = !foundItem.selected
-      // saveSelections()
+    async toggleItemSelection(id) {
+      const foundItem = this.selection.find((item) => item.id === id);
+      foundItem.selected = !foundItem.selected;
+      await saveSelections(this.selection);
+      this.updateVersion();
     },
 
-    isActive(id){
-      const foundItem = this.selection.find(item => item.id === id)
-      return foundItem.selected
-    }
+    isActive(id) {
+      const foundItem = this.selection.find((item) => item.id === id);
+      return foundItem.selected;
+    },
   },
 
-  
-
   async mounted() {
+    await this.fetchData();
 
+    this.createSelectionObject();
 
-
-    await this.fetchData()
-
-    this.createSelectionObject()
-    // this.toggleItemSelection()
-    // this.isActive()
-   
-
-    if(this.getVersion() == null){
-      this.updateVersion()
+    if (this.getVersion() == null) {
+      this.updateVersion();
     }
 
-    this.version = this.getVersion()
+    this.version = this.getVersion();
 
     window.onstorage = async () => {
-      console.log('VERSION CHANGED')
-      this.changedLocation = "there"
-      this.version = this.getVersion()
+      console.log("VERSION CHANGED");
+      this.changedLocation = "there";
+      this.version = this.getVersion();
 
-    //  get data from indexedDB because that changed
-    const newSelection =  await getSelections()
-    this.selection = newSelection.selectionList
-
-//  saveSelections()
+      try {
+        const newSelection = await getSelections();
+        this.selection = newSelection.selectionList;
+      } catch (e) {
+        console.log("ERROR", e);
+      }
     };
-   
   },
 };
 </script>
 
 <template>
-
-
-  <button @click="updateVersion()">Update Version</button>
-  {{version}}
-
-  <p>updated version: <strong>{{this.changedLocation}}</strong></p>
+  <div>
+    <button @click="openIt()">New Window</button>
+  </div>
+  <p>
+    updated version: <strong>{{ this.changedLocation }}</strong>
+  </p>
   <div>
     <ul>
-      <li v-for="(item, index) in myJson" :key="index">{{item.id}} - {{ item.text }}</li>
+      <li v-for="(item, index) in myJson" :key="index">
+        {{ item.id }} - {{ item.text }}
+      </li>
     </ul>
   </div>
 
   <div>
     <ul>
       <li v-for="(item, index) in selection" :key="index">
-        <button class="" type="button" @click="toggleItemSelection(item.id)" :class="{active: isActive(item.id) }">{{item.id}}</button>
+        <button
+          class=""
+          type="button"
+          @click="toggleItemSelection(item.id)"
+          :class="{ active: isActive(item.id) }"
+        >
+          {{ item.id }}
+        </button>
       </li>
     </ul>
   </div>
-
 </template>
 
 <style>
-
-.active{
-  background-color:red;
+.active {
+  background-color: red;
 }
-
 </style>
--- src/utilities/indexedDB-helper.js
+++ src/utilities/indexedDB-helper.js
@@ -1,138 +1,88 @@
+// Create IndexedDB - Set DB-Name and DB-Version
+const DB_NAME = "stateManager";
+const DB_VERSION = 10;
+const SESSION_ID = "blahblah";
+const TABLE_NAME = "selections";
+let DB;
 
-const DB_NAME = 'stateManager'
-const DB_VERSION = 2
-const TABLE_NAME = 'selections'
-let DB
-
-const getDb = async () => new Promise((resolve, reject) => {
-  console.log('call get DB')
-  if (DB) {
-    console.log('exists')
-    resolve(DB)
-  }
-
-  const request = indexedDB.open(DB_NAME, DB_VERSION)
-  console.log("REQUEST", request)
-
-  request.onerror = (event) => {
-    console.log('DB ERROR', event)
-    reject(`Error ${event}`)
-  }
-
-  request.onsuccess = (event) => {
-    console.log('DB SUCCESS', event.target.result)
-    DB = event.target.result
-    resolve(DB)
-  }
-
-  request.onupgradeneeded = (event) => {
-    console.log('DB ON UPDATE NEEDED')
-    const db = (event.target).result
-    try {
-      db.deleteObjectStore(TABLE_NAME)
-    } catch {
-
+export const getDb = async () =>
+  new Promise((resolve, reject) => {
+    if (DB) {
+      resolve(DB);
     }
-    db.createObjectStore(TABLE_NAME, { keyPath: 'selectionId' })
-  }
-})
 
+    const request = indexedDB.open(DB_NAME, DB_VERSION);
+    console.log("REQUEST", request);
 
-export const getSelections = async (sessionId) => {
-  const db = await getDb()
-  let selections = {}
+    request.onerror = (event) => {
+      console.log("DB ERROR", event);
+      reject(`Error ${event}`);
+    };
+
+    request.onsuccess = (event) => {
+      console.log("DB SUCCESS", event.target.result);
+      DB = event.target.result;
+      resolve(DB);
+    };
+
+    request.onupgradeneeded = (event) => {
+      console.log("DB ON UPDATE NEEDED");
+      const db = event.target.result;
+      try {
+        db.deleteObjectStore(TABLE_NAME);
+      } catch {}
+      db.createObjectStore(TABLE_NAME, { keyPath: "selectionId" });
+    };
+  });
+
+// Save new selections to selection array in indexedDB selections table
+export const saveSelections = async (newSelections) => {
+  const db = await getDb();
+  return new Promise(async (resolve, reject) => {
+    try {
+      const note = {
+        selectionId: SESSION_ID,
+        selectionList: JSON.parse(JSON.stringify(newSelections)),
+      };
+
+      const trans = db.transaction(TABLE_NAME, "readwrite");
+      const store = trans.objectStore(TABLE_NAME);
+
+      console.log("ADD TO INDEXEDDB", note);
+      const result = await store.put(note);
+
+      resolve(result);
+    } catch (error) {
+      console.error(`There was an error adding to drafts, ${error}`);
+      reject(error);
+    }
+  });
+};
+
+// Get selections array from selections table in indexedDB
+export const getSelections = async () => {
+  const db = await getDb();
+  let selections = {};
 
   return new Promise((resolve, reject) => {
     try {
-      const trans = db.transaction(TABLE_NAME, 'readonly')
-      const store = trans.objectStore(TABLE_NAME)
-      const request = store.get(sessionId)
+      const trans = db.transaction(TABLE_NAME, "readonly");
+      const store = trans.objectStore(TABLE_NAME);
+      const request = store.get(SESSION_ID);
 
       // assign results to selections when request has been successful
       request.onsuccess = (event) => {
-        selections = event.target.result ?? selections
-        resolve(selections)
-      }
+        selections = event.target.result ?? selections;
+        resolve(selections);
+      };
 
       // close db when request is complete
       request.oncomplete = () => {
-        db.close()
-      }
+        db.close();
+      };
     } catch (error) {
-      // eslint-disable-next-line no-console
-      console.error(`There was an error accessing drafts, ${error}`)
-      reject(error)
+      console.error(`There was an error accessing drafts, ${error}`);
+      reject(error);
     }
-  })
-}
-
-export const saveSelections = async (newSelections) => {
-const db = await getDb()
-  // eslint-disable-next-line no-async-promise-executor
-  return new Promise(async (resolve, reject) => {
-    try {
-      const trans = db.transaction(TABLE_NAME, 'readwrite')
-      const store = trans.objectStore(TABLE_NAME)
-
-      /* Remove observable data added by VUE
-    https://github.com/vuejs/Discussion/issues/292 */
-      // const cleanedUpData = JSON.parse(JSON.stringify(newSelections))
-
-      const note = {
-        selectionId: 2,
-        selectionList: [{id: 1, selected:false}]
-      }
-
-      console.log('ADD TO INDEXEDDB', note)
-      const result = await store.add(note)
-
-
-      // close database when transaction is complete
-      trans.oncomplete = () => {
-        db.close()
-      }
-
-      resolve(result)
-    } catch (error) {
-      // eslint-disable-next-line no-console
-      console.error(`There was an error adding to drafts, ${error}`)
-      reject(error)
-    }
-  })
-}
-
-
-/*
-ATTEMPT TO COPY TUTORIAL LOGIC HERE
- */
-
-
-export const createDBFromTutorial = () => {
-  const request = window.indexedDB.open(DB_NAME, DB_VERSION)
-  console.log('test request', request)
-
-  //on upgrade needed
-  request.onupgradeneeded = e => {
-    DB = e.target.result
-    /* note = {
-         title: "note1",
-         text: "this is a note"
-     }*/
-    const todoNotes = DB.createObjectStore("todo_notes", {keyPath: "title"})
-
-    console.log(`upgrade is called database name: ${DB.name} version : ${DB.version}`)
-
-  }
-  //on success
-  request.onsuccess = e => {
-    DB = e.target.result
-    console.log('DB SUCCESS', event.target.result)
-    console.log(`success is called database name: ${DB.name} version : ${DB.version}`)
-  }
-  //on error
-  request.onerror = e => {
-    console.log('DB SUCCESS', event.target.result)
-    console.log(`error: ${e.target.error} was found `)
-
-  }
-}
+  });
+};
